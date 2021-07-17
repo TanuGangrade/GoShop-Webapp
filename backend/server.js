adaf2dac6c1d4affbb7345352223595b1f291c43
Tanu <tanu.g1234@gmail.com>
2021-07-18 03:21:53 +0400

    prod

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,6 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -32,6 +28,18 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve() // since we usieng import format insted of require , we need to use thsi to get curr directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 
 app.use(notFound)
 app.use(errorHandler)
@@ -39,4 +47,7 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(
-  PORT)
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}` )
+)
